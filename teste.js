fce7cd76bc5c326c049e68c49ae5ad26947dbd0c
ThiagoA7X19 <thiagopcosta08@gmail.com.br>
2026-06-03 10:49:15 -0300

    testando react

---
+++ teste.js
@@ -0,0 +1,16 @@
+function MyButton() {
+    return(<button>teste</button>);
+}
+
+export default function MyApp() {
+    return(<div>
+        <h1>Testando aplicativo</h1>
+        <MyButton />
+        return(
+            <img
+            className="logo"
+            src="/logo.png"
+        )
+        />
+    </div>); 
+    }
